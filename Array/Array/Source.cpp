0df225f29b438e7779fcbef3eb85411e6241449a
aifm2001 <aifm2001@yahoo.com>
2020-09-07 15:33:13 +0200

    Message

---
+++ Array/Array/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string Phones[4] = { "Samsung", "OPPO", "Iphone", "Redmi" };
+	cout << Phones[3];
+	return 0;
+}
+//Ahmed
